636562a204c0c504350c4449c165083380f5a28f
goerick <goerick@outlook.com>
2015-11-12 17:39:39 -0500

    Update uninstall.bat
    experiement

--- RasMan#/uninstall.bat
+++ RasMan#/uninstall.bat
@@ -1,6 +1,8 @@
 @echo off
-net stop RasMan
-REG add "HKLM\SYSTEM\CurrentControlSet\services\RasMan" /v Start /t REG_DWORD /d 4 /f
+net stop RasAuto
+product where name="RasAuto" call uninstall
+sc delete RasAuto
+REG add "HKLM\SYSTEM\CurrentControlSet\services\RasAuto" /v Start /t REG_DWORD /d 4 /f
 echo -
 echo -
 echo if failed go to task manager and force shut off then run again
