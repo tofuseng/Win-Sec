0f5fd501731a978564dded677496db6da7de651f
goerick <goerick@outlook.com>
2015-11-11 19:57:21 -0500

    sleep doesn't work
    changed some stuff

--- Universal Script.bat
+++ Universal Script.bat
@@ -1,20 +1,23 @@
-REM @echo off
+@echo off
 
 echo disabling telnet...
 net stop telnet
 sc config tlntsvr start= disabled
 
-sleep 5
+pause 
 
 echo enabling auto updates...
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 0 /f
 net start wuauserv
 sc config wuauserv start= auto
 
-sleep 5
+pause
 
 echo enabling default windows defender settings
 cd C:\Program Files\Windows Defender
 MpCmdRun.exe -RestoreDefaults
-pause 
+
+echo Click continue to update windows defender
+timeout /t -1 
+
 
