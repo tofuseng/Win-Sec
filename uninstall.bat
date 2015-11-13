a9618c62eb718cc57426921822ca2d1833ecdfa1
goerick <goerick@outlook.com>
2015-11-13 11:46:56 -0500

    Update uninstall.bat
    Added  template

--- uninstall.bat
+++ uninstall.bat
@@ -1,6 +1,20 @@
 @echo off
+
+REM to uninstall a software...
+
 C:\PROGRA~1\Cain\UNINSTAL.EXE C:\PROGRA~1\Cain\Install.log
-echo -
-echo -
+
+
+REM to uninstall a program...
+
+product where name="Name" call uninstall
+sc delete Name
+
+
+REM to disable a registry/startup...
+
+REG add "HKLM\SYSTEM\CurrentControlSet\services\SharedAccess" /v Start /t REG_DWORD /d 4 /f
+
+
 echo if failed go to task manager and force shut off then run again
 pause
