4e78182a10694c1318f7f74ac04089384ea9ea8c
goerick <goerick@outlook.com>
2015-11-13 11:31:55 -0500

    Update UPnP.bat
    Added sc

--- UPnP#/UPnP.bat
+++ UPnP#/UPnP.bat
@@ -1,6 +1,8 @@
 @echo off
 netsh advfirewall firewall set rule group= "upnp mode" new enable=No
 net stop upnphost
+product where name="upnphost" call uninstall
+sc delete upnphost
 REG add "HKLM\SYSTEM\CurrentControlSet\services\upnphost" /v Start /t REG_DWORD /d 4 /f
 echo -
 echo -
