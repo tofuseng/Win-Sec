d291bc62c5044a5ad720c7bf516fa2b5b22aeaad
goerick <goerick@outlook.com>
2015-11-13 11:33:07 -0500

    Update uninstall.bat
    Added sc

--- UmRdpService#/uninstall.bat
+++ UmRdpService#/uninstall.bat
@@ -1,5 +1,7 @@
 @echo off
 net stop UmRdpService
+product where name="UmRdpService" call uninstall
+sc delete UmRdpService
 REG add "HKLM\SYSTEM\CurrentControlSet\services\UmRdpService" /v Start /t REG_DWORD /d 4 /f
 echo -
 echo -
