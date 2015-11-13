8a9660acebe7e7b59fc6873431aef00aaa9aafc9
goerick <goerick@outlook.com>
2015-11-13 11:30:01 -0500

    Update ICS.bat
    Added sc

--- ICS#/ICS.bat
+++ ICS#/ICS.bat
@@ -1,5 +1,7 @@
 @echo off
 net stop SharedAccess
+product where name="SharedAccess" call uninstall
+sc delete SharedAccess
 REG add "HKLM\SYSTEM\CurrentControlSet\services\SharedAccess" /v Start /t REG_DWORD /d 4 /f
 echo -
 echo -
